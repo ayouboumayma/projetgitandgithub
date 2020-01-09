9f0cebab28fe6e738b3f1968c8f353d8d96ee63e
ayouboumayma <59657389+ayouboumayma@users.noreply.github.com>
2020-01-09 23:43:29 +0100

    Add files via upload

---
+++ MyInterface.java
@@ -0,0 +1,15 @@
+package org.GL.gitTest;
+
+
+public interface MyInterface {
+	
+	double prod(double x , double y);
+	double div(double x , double y);
+	double add(double x , double y);
+	double soustr(double x , double y);
+    int Pgcd(int a , int b);
+    int fact(int a);
+    String bin (int a);
+	boolean Estpair(int a);
+    
+}
